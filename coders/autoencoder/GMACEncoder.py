e95e051b3ace8c2ecdcbb9d37a10a60e9e5cf1dc
Georgios Voulgaris <gvsam7@yahoo.com>
2022-08-25 18:39:29 +0100

    debug GMACEncoder

--- coders/autoencoder/GMACEncoder.py
+++ coders/autoencoder/GMACEncoder.py
@@ -7,23 +7,13 @@ class GMACEncoder(nn.Module):
         super(GMACEncoder, self).__init__()
         self.features = nn.Sequential(
             GACBlock(in_channels=colour_channels, out_channels=c, kernel_size=3, padding=1, stride=1),  # 32x32 => 16x16
-            nn.ReLU(inplace=True),
             MixPool(2, 2, 0, 1),
-            nn.BatchNorm2d(c),
             GACBlock(in_channels=c, out_channels=c * 2, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(c * 2),
             GACBlock(in_channels=c * 2, out_channels=c * 4, kernel_size=3, padding=1, stride=1),  # 16x16 => 8x8
-            nn.ReLU(inplace=True),
             MixPool(2, 2, 0, 0.6),
-            nn.BatchNorm2d(c * 4),
             GACBlock(in_channels=c * 4, out_channels=c * 8, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
             MixPool(2, 2, 0, 0.2),
-            nn.BatchNorm2d(c * 8),
             GACBlock(in_channels=c * 8, out_channels=c * 16, kernel_size=3, padding=1, stride=1),  # 8x8 => 4x4
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(c * 16)
         )
         # self.fc = nn.Linear(c*16*4*4, latent_dims)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
